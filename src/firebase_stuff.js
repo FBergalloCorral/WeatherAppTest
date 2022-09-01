eb5239debfe8d28c5951b91a67c7afe1380688c9
Felipe A. Bergallo Corral <correodetrabajodefabc@gmail.com>
2022-09-02 01:19:15 +0200

    Adding Firebase default config

---
+++ src/firebase_stuff.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCD8te_W8T-xvkfiVWJgjlE_wYR1cpQzpg",
+  authDomain: "weatherapptest-e6318.firebaseapp.com",
+  projectId: "weatherapptest-e6318",
+  storageBucket: "weatherapptest-e6318.appspot.com",
+  messagingSenderId: "108353768993",
+  appId: "1:108353768993:web:0bdcca738339ebd4f01d60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
